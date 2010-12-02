ce34e657abd19ef5d28c7f2029d92738286d2ee5
Bernhard Wagner <bernhard.wagner@sbs.ch>
2010-12-02 18:01:17 +0100

    added eror handling and examples

--- utfx.sh
+++ utfx.sh
@@ -10,13 +10,38 @@
 SAXON=lib/saxon9he.jar
 THIRD_PARTY_JARS=
 TRANSFORM_FACTORY= #-Djavax.xml.transform.TransformerFactory=...
-UTFX=.:build/jar/utfxFat.jar
+UTFX_FAT=build/jar/utfxFat.jar
+UTFX=.:$UTFX_FAT
 CLASSES=bin
-UTFX_TEST=-Dutfx.test.file=test_xsl/dtbook2sbsform_test.xml
-UTFX_TEST=-Dutfx.test.dir=test_xsl
+PRGNAME=$0
+
+if [ ! -e $UTFX_FAT ] ; then
+	cat <<END
+
+$UTFX_FAT not yet built. Please run:
+
+ ant fatjar
+	
+before trying again!
+	
+END
+	exit 1
+fi
+
+if [ $# = 0 ] ; then
+	cat <<END
+
+please pass utfx test, either:
+$PRGNAME -Dutfx.test.file=samples/history/test/history_xhtml_test.xml
+or:
+$PRGNAME -Dutfx.test.dir=samples/history
+
+END
+	exit 2
+fi
 
 java \
 	$TRANSFORM_FACTORY \
-	$UTFX_TEST \
+	"$@" \
 	-cp $SAXON:$THIRD_PARTY_JARS:$CLASSES:$UTFX \
 	 utfx.runner.TestRunner utfx.framework.XSLTRegressionTest
